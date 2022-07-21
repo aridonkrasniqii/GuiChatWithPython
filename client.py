d540f0bdb42b3859a22d103c9f7092404a879630
aridonkrasniqii <aridonk16@outlook.com>
2022-07-21 16:45:44 +0200

    cc
    Implementation of server

--- client.py
+++ client.py
@@ -1,18 +1,1 @@
-import socket
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost', 9999))
-
-done = False
-
-while not done:
-    client.send(input('Client: ').encode('utf-8'))
-
-    msg = client.recv(1024).decode('utf-8')
-    if msg == 'quit':
-        done = True
-    else:
-        print("Server: " + msg)
-client.close()
-
-
+import
--- server.py
+++ server.py
@@ -1,19 +1,60 @@
+# clients are going to connect to the server and they can communicate with each other
+# they are not communicating directly client a to client b
+# but they are communicating through the server and the server is broadcasting the messages
 import socket
+import threading
+
+host = 'localhost'
+port = 9999
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost', 9999))
+server.bind((host, port))
 server.listen()
 
-client, addr = server.accept()
+clients = []
+nicknames = []
 
-done = False
 
-while not done:
-    msg = client.recv(1024).decode('utf-8')
-    if msg == 'quit':
-        done = True
-    else:
-        print("Client: " + msg)
-    client.send(input('Server msg: ').encode('utf-8'))
+def broadcast(message):
+    for client in clients:
+        client.send(message)
 
-client.close()
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f'{nicknames[clients.index(client)]} says {message}')
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nicknames.remove(nicknames[index])
+            break
+
+
+def receive():
+    while True:
+        client, addr = server.accept()
+        print(f'Connected with {str(addr)}!')
+
+        client.send("nick".encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print('Nickname of the client is {}'.format(nickname))
+        broadcast(f'{nickname} connected to the server !\n'.encode('utf-8'))
+        client.send('Connected to the server'.encode('utf-8'))
+
+        # client, "comma" because it has to be treated as a tuple
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+
+
+print("Server running ...")
+receive()
